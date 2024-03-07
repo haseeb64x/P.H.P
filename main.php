7265c6342ef52b9f594d937e2107384fc5769d1e
Haseeb <153839458+haseeb64x@users.noreply.github.com>
2024-03-07 20:44:34 +0500

    PHP Class 1

---
+++ main.php
@@ -0,0 +1,83 @@
+<?php
+
+    // Data Types
+
+    // $name = "Hello Haseeb";
+    // $age = 16;
+    // $isCnic = true;
+    // $marks = 80.65;
+
+
+    // echo "$name";
+    // echo "<br>$age";
+    // echo "<br>$isCnic";
+    // echo "<br>$marks";
+
+    // Arithmetic Operation
+
+    // $a = 20;
+    // $b = 30;
+    // $c = $a + $b;
+    // $d = $a - $b;
+    // $e = $a * $b;
+    // $f = $a / $b;
+    // $g = $a % $b;
+
+    // echo  "<br> the sum of $a + $b is :" .$c;
+    // echo  "<br> the sub of $a - $b is :" .$d;
+    // echo  "<br> the mul of $a * $b is :" .$e;
+    // echo  "<br> the div of $a / $b is :" .$f;
+    // echo  "<br> the mod of $a % $b is :" .$g;
+
+    // Increment 
+
+    // $apple = 70;
+    // // Post Increment
+    // echo "<br>" .$apple++ ; // 70
+    // echo "<br>" .$apple ; // 71
+    // // Pre Incremet 
+    // echo "<br>" .++$apple; // 71
+
+    // Initail task
+    // 195
+    // 235
+    // 109
+    // 204
+    // 80
+    // 196
+    // 300
+
+    // $mango = 200;
+    // echo "<br> $mango";
+    // $mango -=5;
+    // echo "<br> $mango";
+    // $mango +=35;
+    // echo "<br> $mango";
+    // $mango -=91;
+    // echo "<br> $mango";
+    // $mango +=4;
+    // echo "<br> $mango";
+    // $mango -=120;
+    // echo "<br> $mango";
+    // $mango -=4;
+    // echo "<br> $mango";
+    // $mango +=100;
+    // echo "<br> $mango";
+
+    // Loops
+    // For Loops
+
+    // for($i=1; $i<=10; $i++){
+    //     echo "<br> This is for loop".$i;
+    // }
+    
+    // for($q=1; $q<=10; $q++){
+    //     echo "<br> 19 x $q = ".$q*19;
+    // }
+
+    $tableNum = 64;
+    for($w=1; $w<=10; $w++){
+        echo "<br> $tableNum x $w = ".$tableNum*$w;
+    }
+    
+?>
