838441fdaca472d762eabe7792f6fe2f054c2098
Haseeb64x <ayeshasidd422@gmail.com>
2024-03-16 17:50:48 +0500

    PHP class 4

---
+++ update.php
@@ -0,0 +1,13 @@
+<?php
+    include("config/connect.php");
+
+    $sql = "Update Users set Name = 'Shapatar Man' where id = 2";
+    $result =$conn->query($sql);
+
+    if($result == true){
+        echo "<br> Data Updated Successfully";
+
+    }else{
+        echo "<br>Data not Updated";
+    }
+?>
