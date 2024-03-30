ffcbf12f4e9a235ee6b3fbd94726d086e929aa1f
Haseeb64x <ayeshasidd422@gmail.com>
2024-03-30 17:36:31 +0500

    30/3/24

---
+++ file/fileupload.php
@@ -0,0 +1,43 @@
+<?php
+    include("../config/connect.php");
+
+    echo"<pre>";
+    print_r($_FILES);
+    echo"</pre>";
+
+    if(isset($_FILES['image'])){
+        $file_name = $_FILES[ 'image' ][ 'name'];
+        $file_type = $_FILES[ 'image' ][ 'type'];
+        $file_size = $_FILES[ 'image' ]['size'];
+        $file_tmp = $_FILES[ 'image' ][ 'tmp_name'];
+
+        move_uploaded_file($file_tmp , "images/" .$file_name);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Upload</title>
+    <style>
+        body{
+            background-color: black;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="form-grp">
+                <label for="">Select your File</label><br>
+                <input type="file" class="form-control" name="images" id="">
+            </div>
+            <input type="submit" value="submit" name="submit" class btn btn-dark>
+        </form>
+    </div>
+</body>
+</html>
